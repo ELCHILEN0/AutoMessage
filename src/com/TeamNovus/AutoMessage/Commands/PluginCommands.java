78b18ebe943c555c560745b647347a5b8cfe4c3d
Jnani <jnaneshwar1234@gmail.com>
2012-11-22 10:40:43 -0800

    Changed around some order and messages

--- src/com/TeamNovus/AutoMessage/Commands/PluginCommands.java
+++ src/com/TeamNovus/AutoMessage/Commands/PluginCommands.java
@@ -27,7 +27,7 @@ public class PluginCommands {
 		sender.sendMessage(ChatColor.GREEN + "Configuration reloaded from disk!");
 	}
 
-	@BaseCommand(aliases = "add", description = "Add  message to a list.", usage = "<List> [Index] [Message]", min = 2)
+	@BaseCommand(aliases = "add", description = "Add a list or message to a list.", usage = "<List> [Index] [Message]", min = 2)
 	public void onAddCmd(CommandSender sender, Command cmd, String commandLabel, String[] args) {
 		// add <List> [Index] [Message]
 		if(!(sender.hasPermission("automessage.commands.add"))) {
@@ -96,7 +96,7 @@ public class PluginCommands {
 		}
 	}
 
-	@BaseCommand(aliases = "remove", description = "Remove a message from the list.", usage = "<List> [Index]", min = 2, max = 3)
+	@BaseCommand(aliases = "remove", description = "Remove a list or message from a list.", usage = "<List> [Index]", min = 2, max = 3)
 	public void onRemoveCmd(CommandSender sender, Command cmd, String commandLabel, String[] args) {
 		// remove <List> [Index]
 		if(!(sender.hasPermission("automessage.commands.remove"))) {
@@ -135,7 +135,72 @@ public class PluginCommands {
 		}
 	}
 
-	@BaseCommand(aliases = "prefix", description = "Set a lists prefix.", usage = "<List> [Prefix]", min = 2)
+	@BaseCommand(aliases = "enabled", description = "Toggle broadcasting for a list.", usage = "<List>", min = 2, max = 2)
+	public void onEnableCmd(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+		// enabled <List>
+		if(!(sender.hasPermission("automessage.commands.enabled"))) {
+			sender.sendMessage(ChatColor.RED + "You do not have permission for this command!");
+			return;
+		}
+
+		if(AutoMessage.getMessageListManager().getBestList(args[1]) != null) {
+			MessageList list = AutoMessage.getMessageListManager().getBestList(args[1]);
+			list.setEnabled(!(list.isEnabled()));
+			sender.sendMessage(ChatColor.GREEN + "Enabled: " + ChatColor.YELLOW + list.isEnabled() + ChatColor.GREEN + "!");
+
+			AutoMessage.getMessageListManager().setList(AutoMessage.getMessageListManager().getBestKey(args[1]), list);
+			AutoMessage.getPlugin().saveConfiguration();
+		} else {
+			sender.sendMessage(ChatColor.RED + "The specified list does not exist!");
+		}
+	}
+
+	@BaseCommand(aliases = "interval", description = "Set a lists broadcast interval.", usage = "<List> <Interval>", min = 3, max = 3)
+	public void onIntervalCmd(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+		// interval <List> <Interval>
+		if(!(sender.hasPermission("automessage.commands.interval"))) {
+			sender.sendMessage(ChatColor.RED + "You do not have permission for this command!");
+			return;
+		}
+
+		if(AutoMessage.getMessageListManager().getBestList(args[1]) != null) {
+			MessageList list = AutoMessage.getMessageListManager().getBestList(args[1]);
+
+			if(StringUtil.isInteger(args[2])) {
+				list.setInterval(Integer.valueOf(args[2]));
+				sender.sendMessage(ChatColor.GREEN + "Interval: " + ChatColor.YELLOW + Integer.valueOf(args[2]) + ChatColor.GREEN + "!");
+
+				AutoMessage.getMessageListManager().setList(AutoMessage.getMessageListManager().getBestKey(args[1]), list);	
+				AutoMessage.getPlugin().saveConfiguration();
+			} else {
+				sender.sendMessage(ChatColor.RED + "The interval must be an Integer!");
+			}
+		} else {
+			sender.sendMessage(ChatColor.RED + "The specified list does not exist!");
+		}
+	}
+
+	@BaseCommand(aliases = "random", description = "Set a lists broadcast method.", usage = "<List>", min = 2, max = 2)
+	public void onRandomCmd(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+		// random <List>
+		if(!(sender.hasPermission("automessage.commands.random"))) {
+			sender.sendMessage(ChatColor.RED + "You do not have permission for this command!");
+			return;
+		}
+
+		if(AutoMessage.getMessageListManager().getBestList(args[1]) != null) {
+			MessageList list = AutoMessage.getMessageListManager().getBestList(args[1]);
+			list.setRandom(!(list.isRandom()));
+			sender.sendMessage(ChatColor.GREEN + "Random: " + ChatColor.YELLOW + list.isRandom() + ChatColor.GREEN + "!");
+
+			AutoMessage.getMessageListManager().setList(AutoMessage.getMessageListManager().getBestKey(args[1]), list);		
+			AutoMessage.getPlugin().saveConfiguration();
+		} else {
+			sender.sendMessage(ChatColor.RED + "The specified list does not exist!");
+		}
+	}
+
+	@BaseCommand(aliases = "prefix", description = "Set a lists prefix for broadcasts.", usage = "<List> [Prefix]", min = 2)
 	public void onPrefixCmd(CommandSender sender, Command cmd, String commandLabel, String[] args) {
 		// prefix <List> <Prefix>
 		if(!(sender.hasPermission("automessage.commands.prefix"))) {
@@ -162,101 +227,6 @@ public class PluginCommands {
 
 				AutoMessage.getMessageListManager().setList(AutoMessage.getMessageListManager().getBestKey(args[1]), list);
 				AutoMessage.getPlugin().saveConfiguration();
-			} else {
-				sender.sendMessage(ChatColor.RED + "The specified list does not exist!");
-			}
-		}
-	}
-
-	@BaseCommand(aliases = "enabled", description = "Toggle broadcasting for a list.", usage = "<List>", min = 2, max = 2)
-	public void onEnableCmd(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-		// enabled <List>
-		if(!(sender.hasPermission("automessage.commands.enabled"))) {
-			sender.sendMessage(ChatColor.RED + "You do not have permission for this command!");
-			return;
-		}
-
-		if(AutoMessage.getMessageListManager().getBestList(args[1]) != null) {
-			MessageList list = AutoMessage.getMessageListManager().getBestList(args[1]);
-			list.setEnabled(!(list.isEnabled()));
-			sender.sendMessage(ChatColor.GREEN + "Enabled: " + ChatColor.YELLOW + list.isEnabled() + ChatColor.GREEN + "!");
-
-			AutoMessage.getMessageListManager().setList(AutoMessage.getMessageListManager().getBestKey(args[1]), list);
-			AutoMessage.getPlugin().saveConfiguration();
-		} else {
-			sender.sendMessage(ChatColor.RED + "The specified list does not exist!");
-		}
-	}
-
-	@BaseCommand(aliases = "interval", description = "Set a lists interval.", usage = "<List> <Interval>", min = 3, max = 3)
-	public void onIntervalCmd(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-		// interval <List> <Interval>
-		if(!(sender.hasPermission("automessage.commands.interval"))) {
-			sender.sendMessage(ChatColor.RED + "You do not have permission for this command!");
-			return;
-		}
-
-		if(AutoMessage.getMessageListManager().getBestList(args[1]) != null) {
-			MessageList list = AutoMessage.getMessageListManager().getBestList(args[1]);
-
-			if(StringUtil.isInteger(args[2])) {
-				list.setInterval(Integer.valueOf(args[2]));
-				sender.sendMessage(ChatColor.GREEN + "Interval: " + ChatColor.YELLOW + Integer.valueOf(args[2]) + ChatColor.GREEN + "!");
-
-				AutoMessage.getMessageListManager().setList(AutoMessage.getMessageListManager().getBestKey(args[1]), list);	
-				AutoMessage.getPlugin().saveConfiguration();
-			} else {
-				sender.sendMessage(ChatColor.RED + "The interval must be an Integer!");
-			}
-		} else {
-			sender.sendMessage(ChatColor.RED + "The specified list does not exist!");
-		}
-	}
-
-	@BaseCommand(aliases = "random", description = "Set a lists broadcasting method.", usage = "<List>", min = 2, max = 2)
-	public void onRandomCmd(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-		// random <List>
-		if(!(sender.hasPermission("automessage.commands.random"))) {
-			sender.sendMessage(ChatColor.RED + "You do not have permission for this command!");
-			return;
-		}
-
-		if(AutoMessage.getMessageListManager().getBestList(args[1]) != null) {
-			MessageList list = AutoMessage.getMessageListManager().getBestList(args[1]);
-			list.setRandom(!(list.isRandom()));
-			sender.sendMessage(ChatColor.GREEN + "Random: " + ChatColor.YELLOW + list.isRandom() + ChatColor.GREEN + "!");
-
-			AutoMessage.getMessageListManager().setList(AutoMessage.getMessageListManager().getBestKey(args[1]), list);		
-			AutoMessage.getPlugin().saveConfiguration();
-		} else {
-			sender.sendMessage(ChatColor.RED + "The specified list does not exist!");
-		}
-	}
-
-	@BaseCommand(aliases = "list", description = "List list or messages in a list.", usage = "[List]", min = 1, max = 2)
-	public void onListCmd(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-		// list [List]
-		if(!(sender.hasPermission("automessage.commands.list"))) {
-			sender.sendMessage(ChatColor.RED + "You do not have permission for this command!");
-			return;
-		}
-
-		if(args.length == 1) {
-			if(AutoMessage.getMessageListManager().getMessageLists().keySet().size() != 0) {
-				sender.sendMessage(ChatColor.DARK_RED + "Availiable Lists:");
-				for(String key : AutoMessage.getMessageListManager().getMessageLists().keySet()) {
-					sender.sendMessage(ChatColor.GOLD + key);
-				}
-			} else {
-				sender.sendMessage(ChatColor.RED + "No lists availiable!");
-			}
-		} else {
-			if(AutoMessage.getMessageListManager().getBestList(args[1]) != null) {
-				sender.sendMessage(ChatColor.DARK_RED + AutoMessage.getMessageListManager().getBestKey(args[1]));
-				List<String> messages = AutoMessage.getMessageListManager().getBestList(args[1]).getMessages();
-				for (int i = 0; i < messages.size(); i++) {
-					sender.sendMessage(ChatColor.YELLOW + "" + i + ": " + ChatColor.RESET + ChatColor.translateAlternateColorCodes("&".charAt(0), AutoMessage.getMessageListManager().getBestList(args[1]).getPrefix() + messages.get(i)));
-				}
 			} else {
 				sender.sendMessage(ChatColor.RED + "The specified list does not exist!");
 			}
@@ -302,6 +272,36 @@ public class PluginCommands {
 			}
 		} else {
 			sender.sendMessage(ChatColor.RED + "The specified list does not exist!");
+		}
+	}
+	
+	@BaseCommand(aliases = "list", description = "List all lists or messages in a list.", usage = "[List]", min = 1, max = 2)
+	public void onListCmd(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+		// list [List]
+		if(!(sender.hasPermission("automessage.commands.list"))) {
+			sender.sendMessage(ChatColor.RED + "You do not have permission for this command!");
+			return;
+		}
+
+		if(args.length == 1) {
+			if(AutoMessage.getMessageListManager().getMessageLists().keySet().size() != 0) {
+				sender.sendMessage(ChatColor.DARK_RED + "Availiable Lists:");
+				for(String key : AutoMessage.getMessageListManager().getMessageLists().keySet()) {
+					sender.sendMessage(ChatColor.GOLD + key);
+				}
+			} else {
+				sender.sendMessage(ChatColor.RED + "No lists availiable!");
+			}
+		} else {
+			if(AutoMessage.getMessageListManager().getBestList(args[1]) != null) {
+				sender.sendMessage(ChatColor.DARK_RED + AutoMessage.getMessageListManager().getBestKey(args[1]));
+				List<String> messages = AutoMessage.getMessageListManager().getBestList(args[1]).getMessages();
+				for (int i = 0; i < messages.size(); i++) {
+					sender.sendMessage(ChatColor.YELLOW + "" + i + ": " + ChatColor.RESET + ChatColor.translateAlternateColorCodes("&".charAt(0), AutoMessage.getMessageListManager().getBestList(args[1]).getPrefix() + messages.get(i)));
+				}
+			} else {
+				sender.sendMessage(ChatColor.RED + "The specified list does not exist!");
+			}
 		}
 	}
 }

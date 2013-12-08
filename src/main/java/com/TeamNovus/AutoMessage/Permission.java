e8e7d29817cb2bfc5dbb875e12343ddb4043b586
Jnani Weibel <jnaneshwar1234@gmail.com>
2013-12-08 09:30:42 -0800

    Different permission system

--- src/main/java/com/TeamNovus/AutoMessage/Permission.java
+++ src/main/java/com/TeamNovus/AutoMessage/Permission.java
@@ -3,36 +3,50 @@ package com.TeamNovus.AutoMessage;
 import org.bukkit.command.CommandSender;
 
 public enum Permission {
-	COMMAND_UPDATE("commands.update"),
-	COMMAND_RELOAD("commands.reload"),
-	COMMAND_ADD("commands.add"),
-	COMMAND_EDIT("commands.edit"),
-	COMMAND_REMOVE("commands.remove"),
-	COMMAND_ENABLE("commands.enable"),
-	COMMAND_INTERVAL("commands.interval"),
-	COMMAND_EXPIRY("commands.expiry"),
-	COMMAND_RANDOM("commands.random"),
-	COMMAND_PREFIX("commands.prefix"),
-	COMMAND_SUFFIX("commands.suffix"),
-	COMMAND_BROADCAST("commands.broadcast"),
-	COMMAND_LIST("commands.list"),
+	COMMAND_UPDATE		("automessage.commands.update"),
+	COMMAND_RELOAD		("automessage.commands.reload"),
+	COMMAND_ADD			("automessage.commands.add"),
+	COMMAND_EDIT		("automessage.commands.edit"),
+	COMMAND_REMOVE		("automessage.commands.remove"),
+	COMMAND_ENABLE		("automessage.commands.enable"),
+	COMMAND_INTERVAL	("automessage.commands.interval"),
+	COMMAND_EXPIRY		("automessage.commands.expiry"),
+	COMMAND_RANDOM		("automessage.commands.random"),
+	COMMAND_PREFIX		("automessage.commands.prefix"),
+	COMMAND_SUFFIX		("automessage.commands.suffix"),
+	COMMAND_BROADCAST	("automessage.commands.broadcast"),
+	COMMAND_LIST		("automessage.commands.list"),
 	NONE("");
 	
 	private String node;
+	private boolean multi;
 	
 	private Permission(String node) {
+		this(node, false);
+	}
+	
+	private Permission(String node, boolean multi) {
 		this.node = node;
+		this.multi = multi;
 	}
 	
 	public String getNode() {
 		return node;
 	}
 	
-	private static String getPermission(Permission permission) {
-		return "automessage." + permission.getNode();
+	public boolean isMulti() {
+		return multi;
+	}
+	
+	public static boolean has(Permission permission, String appending, CommandSender target) {
+		if(appending != null && permission.isMulti()) {
+			return target.hasPermission(permission.getNode() + "." + appending) || target.hasPermission(permission.getNode() + ".*");
+		}
+		
+		return target.hasPermission(permission.getNode());
 	}
 	
 	public static Boolean has(Permission permission, CommandSender target) {
-		return target.hasPermission(getPermission(permission));
+		return has(permission, null, target);
 	}
 }
